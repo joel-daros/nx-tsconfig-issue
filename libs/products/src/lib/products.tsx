d31c3f2c913bdf103fc19ec92ca4e6e26ee8123d
Joel Daros <joel.daros@corteva.com>
2024-05-16 11:09:07 -0500

    clean up

--- libs/products/src/lib/products.tsx
+++ libs/products/src/lib/products.tsx
@@ -1,12 +1,9 @@
-import styles from './products.module.css';
-
 export function Products() {
   const myArr = [1, 2, 3];
   const index: number = 0;
   const value = myArr[index];
   return (
-    <div className={styles.container}>
-      <h1>Welcome to Products!</h1>
+    <div>
       {/* I don't need the optional operator because this project has noUncheckedIndexedAccess = false */}
       <h1>{value.toString()}</h1>
     </div>
